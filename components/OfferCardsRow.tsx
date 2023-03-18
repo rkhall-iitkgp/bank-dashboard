cc02108805b62b0fbadbb23d5b2b823fbf78d5ba
subhajitt <subhajit.cjc@gmail.com>
2023-03-19 04:06:39 +0530

    Offer Cards Row

---
+++ components/OfferCardsRow.tsx
@@ -0,0 +1,62 @@
+import { Image } from '@mantine/core'
+import styled from '@emotion/styled'
+
+const Row = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  width: 100%;
+`
+const OfferCards = styled('div')`
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 8px;
+  padding: 24px 48px;
+  height: 180px;
+  box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);
+  border-radius: 30px;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0px 16px 20px rgba(0, 0, 0, 0.2);
+  }
+`
+
+const CardText = styled('div')`
+  font-family: Montserrat, sans-serif;
+  flex: 1;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 35px;
+  line-height: 43px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+`
+
+const OfferCardsRow = () => {
+  return (
+    <Row>
+      <OfferCards style={{ background: '#0062D6' }}>
+        <CardText style={{ color: '#E7F4F9' }}>Gift Offers</CardText>
+        <Image width={120} height={120} src="/giftbox.png" />
+      </OfferCards>
+
+      <OfferCards style={{ background: '#E7F4F9' }}>
+        <CardText style={{ color: '#0052B3' }}>Get Expert's Help</CardText>
+        <Image width={120} height={120} src="/expert technical support.png" />
+      </OfferCards>
+
+      <OfferCards style={{ background: '#0062D6' }}>
+        <CardText style={{ color: '#E7F4F9' }}>Refer a friend</CardText>
+        <Image width={100} height={100} src="/refer a friend.png" />
+      </OfferCards>
+    </Row>
+  )
+}
+
+export default OfferCardsRow
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,12 @@ export default class _Document extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
