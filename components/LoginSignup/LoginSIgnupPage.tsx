525810e146ffe031402bc4a61dc0b98b2ad26abd
rraj-03 <ritikraj1369@gmail.com>
2023-03-19 01:06:27 +0530

    .

--- components/LoginSignup/LoginSIgnupPage.tsx
+++ components/LoginSignup/LoginSIgnupPage.tsx
@@ -1,48 +1,54 @@
 import {
     createStyles,
+    Text,
+    Title,
     TextInput,
+    Textarea,
     Button,
     Group,
+    ActionIcon,
     rem,
   } from '@mantine/core';
-  import {useState} from 'react'
-//   import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
-//   import { ContactIconsList } from '../ContactIcons/ContactIcons';
+  import { useState } from 'react';
   const useStyles = createStyles((theme) => ({
     wrapper: {
       minHeight: `100vh`,
       boxSizing: 'border-box',
       borderRadius: theme.radius.md,
-      padding: `calc(${theme.spacing.xl} * 2.5)`,
-      [theme.fn.smallerThan('sm')]: {
-        padding: `calc(${theme.spacing.xl} * 1.5)`,
-      },
+    //   padding: `calc(${theme.spacing.xl} * 2.5)`,
+    //   [theme.fn.smallerThan('sm')]: {
+    //     padding: `calc(${theme.spacing.xl} * 1.5)`,
+    //   },
       display: `flex`,
       justifyContent:`center`,
       alignItems:`center`,
+      background:`white`,
+    //   padding:`10px`
     },
     titlebox:{
         marginBottom:`20px`
     },
     title: {
       fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    //   color: theme.black,
+      color: theme.white,
       lineHeight: 1,
       fontWeight:400,
-      paddingBottom:`5px`
+      paddingBottom:`5px`,
+      fontSize:`2rem`
     },
     titlebold: {
       fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    //   color: theme.black,
+      color: `#1673E1`,
       lineHeight: 1,
-      fontWeight:600,
-      fontSize:`1.5rem`
+      fontWeight:400,
+      fontSize:`3rem`,
+      textAlign:`center`
     },
   
     description: {
       color: theme.colors[theme.primaryColor][0],
       maxWidth: rem(300),
-  
+        fontSize:`0.8rem`,
       [theme.fn.smallerThan('sm')]: {
         maxWidth: '100%',
       },
@@ -51,12 +57,19 @@ import {
     form: {
       backgroundColor: theme.white,
       padding: theme.spacing.xl,
-      borderRadius: theme.radius.md,
-      boxShadow: theme.shadows.lg,
-      width:`300px`,
-      color:`#0052B3`
+      width:`400px`,
+      color:`#0052B3`,
+      display:`flex`,
+      margin:`auto`
     },
-  
+    sideContainer:{
+        
+        width:`100%`,
+        height:`100%`,
+        minHeight:`100vh`,
+        padding:`2vh`,
+        
+    },
     social: {
       color: theme.white,
   
@@ -76,7 +89,8 @@ import {
       border:'0',
       borderRadius:"0",
       background:'transparent',
-      borderBottom:`2px solid #eee`
+      borderBottom:`2px solid #eee`,
+      marginTop:`1rem`
     },
   
     inputLabel: {
@@ -93,58 +107,82 @@ import {
   
     control: {
       backgroundColor: `#006AE4`,
-      borderRadius:`20px`
+      borderRadius:`20px`,
+      margin:`1rem`,
+      width:`300px`
     },
     forminside:{
         maxWidth:`80%`,
         width:`300px`,
         margin:`auto`
     },
+    grid:{
+        display:`grid`,
+        gridTemplateColumns:`40% 60%`,
+        width:`100%`,
+        height:`100%`,
+    },
+    sidecontainerinside:{
+        background:`#006BE5`,
+        width:`100%`,
+        padding:'50px',
+        paddingTop:`100px`,
+        height:`96vh`,
+        borderRadius: theme.radius.md,
+        boxShadow: theme.shadows.lg,
+    },
+    buttoncontainer:{
+        display:`flex`,
+        justifyContent:`space-around`
+    },
+    button:{
+        width:`100px`,
+        backgroundColor: `black`,
+        borderRadius:`20px`,
+    }
   }));
   
 //   const social = [IconBrandTwitter, IconBrandYoutube, IconBrandInstagram];
   
-  export function ContactUs() {
+  export function LoginSIgnupPage() {
     const { classes } = useStyles();
-  
+    const [otp, setOtp] = useState(false)
     // const icons = social.map((Icon, index) => (
     //   <ActionIcon key={index} size={28} className={classes.social} variant="transparent">
     //     <Icon size="1.4rem" stroke={1.5} />
     //   </ActionIcon>
     // ));
-  const [otp, setOtp] = useState(false)
+  
     return (
       <div className={classes.wrapper}>
-        
+        <div className={classes.grid}>
+          
           <div className={classes.form}>
-            <div className={classes.forminside}>
+          <div className={classes.forminside}>
             <div className={classes.titlebox}>
-            <div className={classes.title}>Add Account</div>
-            <div className={classes.titlebold}>Enter Your Details</div>
+            <div className={classes.titlebold}>Shiftbank</div>
             </div>
             
             <TextInput
-              placeholder="Account Number"
+              placeholder="Mobile Number"
               type={"number"}
               required
               classNames={{ input: classes.input, label: classes.inputLabel,root: classes.inputcontainer }}
             />
             <TextInput
-              placeholder="Mobile Number"
-              type={"number"}
+              placeholder="Email"
+              type={"email"}
               mt="md"
               classNames={{ input: classes.input, label: classes.inputLabel,root: classes.inputcontainer }}
+              required
+
             />
-            <TextInput
-              placeholder="IFSC"
-              mt="md"
-              classNames={{ input: classes.input, label: classes.inputLabel,root: classes.inputcontainer }}
-            />
+            
             {
                 otp?<TextInput
                 placeholder="OTP"
               type={"number"}
-                
+                required
                 mt="md"
                 classNames={{ input: classes.input, label: classes.inputLabel,root: classes.inputcontainer }}
               />:<></>
@@ -155,8 +193,27 @@ import {
                 setOtp(true)
               }}>{otp?"Confirm":"Get OTP"}</Button>
             </Group>
+            <div className={classes.buttoncontainer}>
+            <Button className={classes.button} onClick={()=>{
+              }}>Sign Up</Button>
+              <Button className={classes.button} onClick={()=>{
+              }}>Sign In</Button>
+            </div>
             </div>
           </div>
+          <div className={classes.sideContainer}>
+            <div className={classes.sidecontainerinside}>
+            <Title className={classes.title}>A Comprehensive Analysis of your Transactions</Title>
+            <Text className={classes.description} mt="sm" mb={30}>
+              Leave your email and we will get back to you within 24 hours
+            </Text>
+  
+            {/* <ContactIconsList variant="white" /> */}
+  
+            {/* <Group mt="xl">{icons}</Group> */}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { ContactUs } from '../components/AddAcoount'
+import { LoginSIgnupPage } from '../components/LoginSignup/LoginSIgnupPage'
+import { OTP } from '../components/otp'
+import { PayBenficiary } from '../components/PayBenificiary'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <ContactUs/>
+      {/* <ContactUs/> */}
+      {/* <LoginSIgnupPage/> */}
+      {/* <PayBenficiary sbi={true}/> */}
+      <OTP/>
     </div>
   )
 }
