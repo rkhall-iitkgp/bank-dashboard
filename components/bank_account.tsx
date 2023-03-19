15365145c6b1538b3bbb3f7197b5adcb2ba308b4
shau8122 <97349130+shau8122@users.noreply.github.com>
2023-03-19 22:35:50 +0530

    Update bank_account.tsx

--- components/bank_account.tsx
+++ components/bank_account.tsx
@@ -1,110 +1,149 @@
-import styled from '@emotion/styled';
-import { Card, Container } from '@mantine/core';
-import { Image } from '@mantine/core';
-import { Text, } from '@mantine/core';
-import {Box, CSSObject, rem , Flex, Button, MantineProvider} from '@mantine/core';
+import styled from "@emotion/styled";
+import {
+  ButtonProps,
+  createPolymorphicComponent
+  
+} from "@mantine/core";
+import { Image } from "@mantine/core";
+import logo from "./sbi 1.png";
+import circle from "./Frame .png";
+import { Text } from "@mantine/core";
+import {
+  // Box, CSSObject, rem , Flex,
+  Button,
+} from "@mantine/core";
 
 const Oflex = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 90vw;
-    flex-wrap: wrap;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
 `;
 
 const Iflex = styled.div`
-    width: 90vw;
-    padding: 10px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
+  margin: 10px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-const StyledButton = styled(Button)`
-    width: 332px;
-    height: 62px;
-    background: #0062D6;
-    border-radius: 30px;
-    margin-right: 40px;
-    margin-top: 21px;
+const _StyledButton = styled(Button)`
+  height: 5vh;
+  background: #0062d6;
+  border-radius: 30px;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 30px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #ffffff;
+  margin-right: 50px;
+`;
+const StyledButton = createPolymorphicComponent<"button", ButtonProps>(
+  _StyledButton
+);
+const StyledImage = styled.div`
+  width: 140px;
+  height: 48px;
+`;
+const StyledImage1 = styled.div`
+  width: 140px;
+  height: 140px;
 `;
 
-const StyledCard = styled( Card )`
-    width: 211px;
-    height: 231px;
-    background: rgba(0, 82, 179, 0.1);
-    border: 2px solid #0052B3;
-    border-radius: 30px;
-    margin-left: 46px;
-    margin-top: 40px;
-    margin-bottom: 40px;
+const StyledCard = styled.div`
+  width: 211px;
+  height: 231px;
+  background: rgba(0, 82, 179, 0.1);
+  border: 2px solid #0052b3;
+  border-radius: 30px;
+  margin: 20px 20px 20px 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 
-const StyledAddCard = styled( Card )`
-    width: 211px;
-    height: 231px;
-    background: rgba(0, 82, 179, 0.1);
-    border: 2px dashed #0052B3;
-    border-radius: 30px;
-    margin-left: 46px;
-    margin-top: 40px;
-    margin-bottom: 40px;
+const StyledAddCard = styled.div`
+  width: 211px;
+  height: 231px;
+  background: rgba(0, 82, 179, 0.1);
+  border: 2px dashed #0052b3;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
 `;
-
 
 const StyledContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 90vw;
-    margin-left: 46px;
-    margin-top: 82px;
-    border-radius: 30px; 
-    box-shadow: 0px 12px 40px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  width: 96vw;
+  margin: 0 2vw;
+  border-radius: 30px;
+  box-shadow: 0px 12px 40px rgba(0, 0, 0, 0.2);
 `;
 
-const StyledText = styled(Text)`
-    width: 435px;
-    height: 79px;
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 60;
-    font-size: 40px;
-    line-height: 49px;
-    color: #0052B3;
-    margin-left: 40px;
-    margin-top: 21px;
-    padding: 10px;
+const StyledText = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.5rem;
+  line-height: 49px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin-left: 30px;
+  color: #0052b3;
+`;
+const StyledText1 = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.5rem;
+  line-height: 49px;
+  text-align: center;
+  color: black;
 `;
 
-const Styledplus = styled(Text)`
-    width: 45px;
-    height: 45px;
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 60px;
-    line-height: 73px;
-    color: rgba(0, 82, 179, 0.73);
-`;
+const Styledplus = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.3rem;
+  line-height: 1.5rem;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #0052b3;`;
 
-export default function Demo() {
-    return (
-        <StyledContainer>
-            <Iflex>
-                <StyledText>Add Bank Account</StyledText>
-                <StyledButton>Add Account</StyledButton>
-            </Iflex>
-            <Oflex>
-                <StyledCard>
-                    <Image src={logo} />
-                    <Text>STATE BANK OF INDIA</Text>
-                    <Text>9875</Text>
-                </StyledCard>
-                <StyledAddCard>
-                </StyledAddCard>
-            </Oflex>
-        </StyledContainer>
-);
+export default function BankAccount() {
+  return (
+    <StyledContainer>
+      <Iflex>
+        <StyledText>Add Bank Account</StyledText>
+        <StyledButton>Add Account</StyledButton>
+      </Iflex>
+      <Oflex>
+        <StyledCard>
+          <StyledImage>
+            <Image src={logo} />
+          </StyledImage>
+          <Styledplus>STATE BANK OF INDIA</Styledplus>
+          <StyledText1>9878</StyledText1>
+        </StyledCard>
+        <StyledAddCard>
+            <StyledImage1>
+            <Image src={circle} />
+            </StyledImage1>
+        </StyledAddCard>
+      </Oflex>
+    </StyledContainer>
+  );
 }
-
