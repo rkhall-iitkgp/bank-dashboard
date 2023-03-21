5df57775c992052c3cb5550f172188c109738707
samar-1601 <samar.pp.singh@gmail.com>
2023-03-21 10:11:50 +0530

    removed home module css import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from 'next'
 import { LoginSignupPage } from '../components/login-signup/LoginSignupPage'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <LoginSignupPage />
-    </div>
-  )
+  return <LoginSignupPage />
 }
 
 export default Home
