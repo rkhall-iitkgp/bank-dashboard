96fc297fe15474dd509f6aa22cc298effae7fe15
samar-1601 <samar.pp.singh@gmail.com>
2023-03-21 23:33:50 +0530

    fixed add bank account flow

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,24 +1,39 @@
 import type { NextPage } from 'next'
 import BankAccount from '../../components/home/add-bank-account/bank_account'
 import Payment from '../../components/home/make-payment-section'
-import Navbar from '../../components/navbar'
+import Navbar from '../../components/home/navbar/navbar'
 import OfferCardsRow from '../../components/home/offers-referal-section/OfferCardsRow'
 import SeeYourAnalysis from '../../components/home/see-your-analysis-section/SeeYourAnalysis'
 import { useState } from 'react'
-import { ContactUs } from '../../components/add-bank-account-flow/AddAccount'
+import { AddAccountFormPopup } from '../../components/home/add-bank-account/AddAccountFormPopup'
 
 const Home: NextPage = () => {
-  const [addAccount, setAddAccount] = useState(false)
-
+  const [bankAccountList, setBankAccountList] = useState<any[]>([
+    { account_no: 1297 },
+  ])
+  const [isAddAccountPopupOpen, setIsAddAccountPopupOpen] =
+    useState<boolean>(false)
   return (
-    <div>
+    <>
       <Navbar />
       <SeeYourAnalysis />
       <Payment />
-      <BankAccount setAddAccount={setAddAccount} />
+      <BankAccount
+        bankAccountList={bankAccountList}
+        setIsAddAccountPopupOpen={setIsAddAccountPopupOpen}
+      />
       <OfferCardsRow />
-      {addAccount ? <ContactUs setAddAccount={setAddAccount} /> : <></>}
-    </div>
+      {isAddAccountPopupOpen ? (
+        <AddAccountFormPopup
+          bankAccountList={bankAccountList}
+          isAddAccountPopupOpen={isAddAccountPopupOpen}
+          setIsAddAccountPopupOpen={setIsAddAccountPopupOpen}
+          setBankAccountList={setBankAccountList}
+        />
+      ) : (
+        <></>
+      )}
+    </>
   )
 }
 
