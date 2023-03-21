58308e2388cda99a762c5a448e9f018927fbe2a6
samar-1601 <samar.pp.singh@gmail.com>
2023-03-21 09:48:49 +0530

    fixed typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { LoginSignupPage } from '../components/login-signup/LoginSIgnupPage'
+import { LoginSignupPage } from '../components/login-signup/LoginSignupPage'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
