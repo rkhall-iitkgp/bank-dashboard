dff5a788c726632704f7b99c5a5671986c4ee360
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2023-03-22 16:59:43 +0530

    added financial charts

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -4,6 +4,8 @@ import styled from "@emotion/styled";
 import RecentTransactions from "./recenttransactions";
 import CashCard from "./cashcard";
 import EodBalance from "./eodBalance";
+import FinancialStatistics from "./statistics";
+import { useState } from "react";
 
 const AccountSelect = styled(Group)`
     border-radius: 30px;
@@ -12,10 +14,13 @@ const AccountSelect = styled(Group)`
 `
 
 const Dashboard = () => {
-    return (<div>
+    const [depositLimit, setDepositLimit] = useState(1000);
+    const [withdrawlLimit, setWithdrawlLimit] = useState(1000);
+
+    return (<div style={{ backgroundColor: "#F4F4F4" }}>
         <Navbar />
 
-        <Group className="dashboard-group">
+        <Group className="dashboard-group" align={"flex-start"} pt={20}>
             <Stack className="left-side" mx={20}>
                 <Group my={10} style={{ justifyContent: "space-between" }}>
                     <Card h={50} w={50} bg={"#0062D6"} p={10} radius={50} ml={15}>
@@ -28,8 +33,8 @@ const Dashboard = () => {
                 </Group>
 
                 <Group style={{ justifyContent: "space-between" }}>
-                    <CashCard n={5} type={"deposit"} />
-                    <CashCard n={10} type={"withdrawl"} />
+                    <CashCard n={5} type={"deposit"} limit={depositLimit} setLimit={setDepositLimit} />
+                    <CashCard n={10} type={"withdrawl"} limit={withdrawlLimit} setLimit={setWithdrawlLimit} />
                 </Group>
 
                 <EodBalance balance="$1,23,456" comparision={4.6} />
@@ -38,7 +43,12 @@ const Dashboard = () => {
             </Stack>
 
             <Stack className="right-side">
+                <Group mx={20}>
+                    <Text fz={35} fw={700} ff="Montserrat">Welcome Back,</Text>
+                    <Text fz={35} fw={700} c={"#0062D6"} ff="Montserrat">Bill Gates!</Text>
+                </Group>
 
+                <FinancialStatistics />
             </Stack>
         </Group>
     </div>)
