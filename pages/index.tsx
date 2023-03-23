b3bf87ed0a90d0801f01a1a37698e90d70335c68
Utkarsh <utkarshbhatt@kgpian.iitkgp.ac.in>
2023-03-23 17:03:03 +0530

    modified index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,8 @@
 import type { NextPage } from 'next'
-<<<<<<< HEAD
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Dashboard from './dashboard'
-
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-
-      {/* <ContactUs/> */}
-      {/* <LoginSIgnupPage /> */}
-      <Dashboard />
-      {/* <PayBenficiary sbi={true}/> */}
-      {/* <OTP /> */}
-      {/* <Banktransfer/> */}
-      {/* <BeneficiaryBank/> */}
-      {/* <UpiTransferHome /> */}
-      {/* <Upitransfer3/> */}
-      {/* <Reviewdetailsupi/> */}
-      {/* <Reviewdetailsbank sbi={false} /> */}
-    </div>
-  )
-=======
 import { LoginSignupPage } from '../components/login-signup/LoginSignupPage'
 
 const Home: NextPage = () => {
   return <LoginSignupPage />
->>>>>>> main
 }
 
 export default Home
