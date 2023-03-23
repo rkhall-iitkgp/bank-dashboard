afef1e29ac73b615cb22fa93279fcfc3448f2bde
Adarsh-51 <108488189+Adarsh-51@users.noreply.github.com>
2023-03-23 20:19:04 +0530

    Transactions_Page

---
+++ pages/transactions.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TransactionForm } from '../components/TransactionForm'
+
+const transactions = () => {
+    return (
+        <TransactionForm/>
+    )
+}
+
+export default transactions
