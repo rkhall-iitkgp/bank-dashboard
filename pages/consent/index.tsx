02dc44643895583fd6100565c8ef0e9a05f6fc6f
samar-1601 <samar.pp.singh@gmail.com>
2023-03-26 04:03:13 +0530

    removed build error

--- pages/consent/index.tsx
+++ pages/consent/index.tsx
@@ -1,30 +1,29 @@
 import { createStyles } from '@mantine/core'
 import React from 'react'
 import ConsentFormComponent from '../../components/profile/ConsentFormComponent'
-const useStyles = createStyles(() => ({
-    wraper: {
-        width: `100%`,
-        minHeight: `100vh`,
-        height: `100%`,
-        boxSizing: 'border-box',
-        // padding: `calc(${theme.spacing.xl} * 2.5)`,
-        // [theme.fn.smallerThan('sm')]: {
-        //   padding: `calc(${theme.spacing.xl} * 1.5)`,
-        // },
-        display: `flex`,
-        justifyContent: `center`,
-        alignItems: `center`,
-        backgroundColor: `#EEEEEE`
-    }
+const useStyles = createStyles((theme) => ({
+  wraper: {
+    width: `100%`,
+    minHeight: `100vh`,
+    height: `100%`,
+    boxSizing: 'border-box',
+    // padding: `calc(${theme.spacing.xl} * 2.5)`,
+    // [theme.fn.smallerThan('sm')]: {
+    //   padding: `calc(${theme.spacing.xl} * 1.5)`,
+    // },
+    display: `flex`,
+    justifyContent: `center`,
+    alignItems: `centuer`,
+    backgroundColor: `#EEEEEE`,
+  },
 }))
-const index = () => {
-    const { classes } = useStyles()
-    return (
-        <div className={classes.wraper}>
-            <ConsentFormComponent />
-
-        </div>
-    )
+const Consent = () => {
+  const { classes } = useStyles()
+  return (
+    <div className={classes.wraper}>
+      <ConsentFormComponent />
+    </div>
+  )
 }
 
-export default index
+export default Consent
