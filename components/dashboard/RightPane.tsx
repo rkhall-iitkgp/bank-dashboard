4a64d41d7ce1687d205b329751828fccdc65d04d
Adarsh-51 <108488189+Adarsh-51@users.noreply.github.com>
2023-03-27 01:41:35 +0530

    .

--- components/dashboard/RightPane.tsx
+++ components/dashboard/RightPane.tsx
@@ -1,9 +1,11 @@
 import { Stack, Group, Text } from '@mantine/core'
+import useStorage from '../../hooks/useStorage'
 import { FinancialRatios } from './FinancialRatios'
 import FinancialStatistics from './statistics'
 import { TotalBalance } from './TotalBalance'
 
 const RightPane = () => {
+  const {getItem} = useStorage()
   return (
     <>
       <Stack className="right-side" style={{ flex: 2.5 }}>
@@ -12,7 +14,7 @@ const RightPane = () => {
             Welcome Back,
           </Text>
           <Text fz={35} fw={700} c={'#0062D6'} ff="Montserrat">
-            Bill Gates!
+            {getItem('name')+"!"}
           </Text>
         </Group>
         <Group>
