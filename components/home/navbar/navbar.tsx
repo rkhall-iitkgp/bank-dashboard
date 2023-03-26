3eb24564cc5517246632000917d046db62d7d5eb
Yash Kumar <yash23kr@kgpian.iitkgp.ac.in>
2023-03-26 14:29:44 +0530

    Design changes

--- components/home/navbar/navbar.tsx
+++ components/home/navbar/navbar.tsx
@@ -10,7 +10,7 @@ const StyledNavbar = styled.nav`
   height: 4rem;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
   width: 100%;
-  width:100%;
+  width: 100%;
   position: sticky;
   top: 0;
   z-index: 100;
