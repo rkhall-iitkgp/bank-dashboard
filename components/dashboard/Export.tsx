7015d23f0ef419c14b08c9267a2b8ff774076d51
Utkarsh <utkarshbhatt@kgpian.iitkgp.ac.in>
2023-04-01 02:49:50 +0530

    pulled

---
+++ components/dashboard/Export.tsx
@@ -0,0 +1,18 @@
+// // Using a class component, everything works without issue
+// import React from "react";
+// export class ComponentToPrint extends React.PureComponent {
+//     render() {
+//       return (
+//         <div>My cool content here!</div>
+//       );
+//     }
+//   }
+  
+//   // Using a functional component, you must wrap it in React.forwardRef, and then forward the ref to
+//   // the node you want to be the root of the print (usually the outer most node in the ComponentToPrint)
+//   // https://reactjs.org/docs/refs-and-the-dom.html#refs-and-function-components
+//   export const ComponentToPrint = React.forwardRef((props, ref) => {
+//     return (
+//       <div ref={ref}>My cool content here!</div>
+//     );
+//   });
--- components/dashboard/ExportButton.tsx
+++ components/dashboard/ExportButton.tsx
@@ -1,78 +1,78 @@
-import { Button, Select, Menu, createStyles } from '@mantine/core'
-import styled from '@emotion/styled'
-import React, { useState } from 'react'
-import useAccountStore from '../Store/Account'
+// import { Button, Select, Menu, createStyles } from '@mantine/core'
+// import styled from '@emotion/styled'
+// import React, { useState } from 'react'
+// import useAccountStore from '../Store/Account'
 // import { useReactToPrint } from 'react-to-print'
 
-const useStyles = createStyles((theme) => ({
-  button: {
-    width: `120px`,
-    background: `#0062D6`,
-    color: `white`,
-    borderRadius: `50px`,
-    display: `flex`,
-    flexDirection: `row`,
-    alignItems: `center`,
-    justifyContent: `space-around`,
-  },
-}))
+// const useStyles = createStyles((theme) => ({
+//   button: {
+//     width: `120px`,
+//     background: `#0062D6`,
+//     color: `white`,
+//     borderRadius: `50px`,
+//     display: `flex`,
+//     flexDirection: `row`,
+//     alignItems: `center`,
+//     justifyContent: `space-around`,
+//   },
+// }))
 
-export default function ExportButton() {
-  const { classes } = useStyles()
-  const [selected, setSelected] = useState()
-  const accounts = useAccountStore()
+// export default function ExportButton() {
+//   const { classes } = useStyles()
+//   const [selected, setSelected] = useState()
+//   const accounts = useAccountStore()
 
-  const handleExportJSON = () => {
-    const jsonStr = JSON.stringify(accounts)
-    const dataUri =
-      'data:text/json;charset=utf-8,' + encodeURIComponent(jsonStr)
-    const link = document.createElement('a')
-    link.setAttribute('href', dataUri)
-    link.setAttribute('download', 'accounts.json')
-    document.body.appendChild(link)
-    link.click()
-  }
+//   const handleExportJSON = () => {
+//     const jsonStr = JSON.stringify(accounts)
+//     const dataUri =
+//       'data:text/json;charset=utf-8,' + encodeURIComponent(jsonStr)
+//     const link = document.createElement('a')
+//     link.setAttribute('href', dataUri)
+//     link.setAttribute('download', 'accounts.json')
+//     document.body.appendChild(link)
+//     link.click()
+//   }
 
-  // const ComponentToPrint = () => {
-  //   return (
-  //     <div>
-  //       <h1>Hello, World!</h1>
-  //       <p>This is a sample text.</p>
-  //     </div>
-  //   )
-  // }
+//   const ComponentToPrint = () => {
+//     return (
+//       <div>
+//         <h1>Hello, World!</h1>
+//         <p>This is a sample text.</p>
+//       </div>
+//     )
+//   }
 
-  // const componentRef = React.useRef()
+//   const componentRef = React.useRef()
 
-  // const handleExportPDF = useReactToPrint({
-  //   content: () => componentRef.current,
-  // })
+//   const handleExportPDF = useReactToPrint({
+//     content: () => componentRef.current,
+//   })
 
-  return (
-    <Menu shadow="md" width={200}>
-      <Menu.Target>
-        <Button className={classes.button}>
-          Export
-          <img
-            src="/icons/exportb.png"
-            alt="export"
-            width="18px"
-            height="18px"
-            style={{ marginLeft: `10px` }}
-          />
-        </Button>
-      </Menu.Target>
+//   return (
+//     <Menu shadow="md" width={200}>
+//       <Menu.Target>
+//         <Button className={classes.button}>
+//           Export
+//           <img
+//             src="/icons/exportb.png"
+//             alt="export"
+//             width="18px"
+//             height="18px"
+//             style={{ marginLeft: `10px` }}
+//           />
+//         </Button>
+//       </Menu.Target>
 
-      <Menu.Dropdown>
-        <Menu.Item>Export as PDF</Menu.Item>
-        {/* <Menu.Item onClick={handleExportCSV}>Export as CSV</Menu.Item> */}
-        <Menu.Item onClick={handleExportJSON}>Export as JSON</Menu.Item>
-        <Menu.Item>Send an Email</Menu.Item>
-      </Menu.Dropdown>
+//       <Menu.Dropdown>
+//         <Menu.Item>Export as PDF</Menu.Item>
+//         {/* <Menu.Item onClick={handleExportCSV}>Export as CSV</Menu.Item> */}
+//         <Menu.Item onClick={handleExportJSON}>Export as JSON</Menu.Item>
+//         <Menu.Item>Send an Email</Menu.Item>
+//       </Menu.Dropdown>
 
-      {/* <div style={{ display: 'none' }}>
-        <ComponentToPrint ref={componentRef} />
-      </div> */}
-    </Menu>
-  )
-}
+//       <div style={{ display: 'none' }}>
+//         <ComponentToPrint ref={componentRef} />
+//       </div>
+//     </Menu>
+//   )
+// }
